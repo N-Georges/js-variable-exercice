7ba17b32991c5d8ec46b8d8f4b5d87cca6fa6a89
georges <gf.nodari@hotmail.com>
2021-10-02 17:19:37 +0200

    project init

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+
+let numberOne = 100;
+let numberTwo = 10;
+
+////////addition/////////
+let addi = numberOne + numberTwo;
+console.log(` ${numberOne} + ${numberTwo} = ${addi}`);
+////////soustraction/////
+let sous = numberOne - numberTwo;
+console.log(`${numberOne} - ${numberTwo} = ${sous}`);
+////////division///////
+let division = numberOne / numberTwo;
+console.log(`${numberOne} / ${numberTwo} = ${division}`);
+////////multiplication/////
+let multiplication = numberOne * numberTwo;
+console.log(`${numberOne} * ${numberTwo} = ${multiplication}`);
+
+
+
+///////////////Excercice 2 //////////////////////
+ let age = parseInt(prompt("Quel est votre age"));
+ let prenom = prompt("Quel est votre prenom");
+ console.log(`${prenom} a ${age} ans cette année`);
+
+ console.log(`${prenom} aura ${age +3} dans 3 ans `);
+
+
+/////////////Excercice 3 ////////////////////////
+let naissance = 1997;
+let anneeActu = 2021;
+let ageActu = anneeActu - naissance;
+console.log(`tu a ${ageActu} ans`);
+console.log(`tu aura ${ageActu + 2} 2 ans apres l'année actuelle`);
+
+
+
+
+
